95505b0d9af338e9c9301b1fb0aa9869e12d387e
Jester572 <jester572@yahoo.com>
2023-02-03 16:15:26 -0700

    Bom

---
+++ lesson5/scripts/script.js
@@ -0,0 +1,27 @@
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+
+
+button.addEventListener("click", function() {
+
+   
+
+    const list_item = document.createElement("li");
+    const list_text = document.createElement("span");
+    const list_button = document.createElement("buttton");
+
+    list_item.appendChild(list_text);
+    list_text.textContent = input.value;
+    list_item.appendChild(list_button);
+    list_button.textContent = "❌";
+
+    list.appendChild(list_item);
+
+    input.value = "";
+
+    list_button.addEventListener("click", function () {
+        list.removeChild(list_item);
+        });
+
+});
