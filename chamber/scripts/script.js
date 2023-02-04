7ea3cc282f111813f928fb5467f61fe9b84d707d
Jester572 <jester572@yahoo.com>
2023-02-03 20:45:26 -0700

    Finnished my site!!!

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -11,8 +11,13 @@ hamburger.addEventListener("click", () => {if(hamburger.innerHTML == "X"){hambur
 
 //updateTime
 const addDate = document.querySelector(".timeNow");
-const now = new Date();
+ now = new Date('August 19, 1975 23:15:30');
 const fulldate = new Intl.DateTimeFormat("en-US",{dateStyle: "full"}).format(now);
 
+addDate.innerHTML = `${fulldate}`;
 
-addDate.innerHTML = `${fulldate}`; 
+// join us banner at top of page
+const join = document.querySelector(".join") 
+if (now.getDay() == 1 || now.getDay() == 2) {
+    join.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+}
