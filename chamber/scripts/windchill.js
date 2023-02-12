31e98e3c256be10abf9b1180e24d5e4850ec114d
Jester572 <jester572@yahoo.com>
2023-02-11 21:29:02 -0700

    made windchill to 1 decimal

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -5,4 +5,4 @@ const chillDiv = document.querySelector("#windChill span");
 let windChill = 35.74 + (0.6215 * temperature.innerHTML) - (35.755 * windSpeed.innerHTML ** 0.16) + 0.4275 * temperature.innerHTML * windSpeed.innerHTML ** 0.16;
 console.log(windChill);
 
-chillDiv.textContent = windChill.toFixed(2);
+chillDiv.textContent = windChill.toFixed(1);
