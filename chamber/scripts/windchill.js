dcca51eff55bf80368737e3c032cb0495f2e20fa
Jester572 <jester572@yahoo.com>
2023-02-11 22:08:37 -0700

    Finnished the weather

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,8 +1,13 @@
-const temperature = document.querySelector("#temp span");
+let temperature = document.querySelector("#temp span");
 const windSpeed = document.querySelector("#windSpeed span");
 const chillDiv = document.querySelector("#windChill span");
 
 let windChill = 35.74 + (0.6215 * temperature.innerHTML) - (35.755 * windSpeed.innerHTML ** 0.16) + 0.4275 * temperature.innerHTML * windSpeed.innerHTML ** 0.16;
-console.log(windChill);
 
-chillDiv.textContent = windChill.toFixed(1);
+if (temperature.innerHTML <= 50 && windSpeed.innerHTML > 3) {
+    chillDiv.textContent = windChill.toFixed(1) + " °F";
+} else {
+    chillDiv.textContent = "N/A";
+}
+
+
