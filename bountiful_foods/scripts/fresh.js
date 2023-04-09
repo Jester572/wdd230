71a3799d4072a5768c32e5776c9a218dee330c81
Jester572 <jester572@yahoo.com>
2023-04-09 04:13:19 -0600

    Calling it all good

--- bountiful_foods/scripts/fresh.js
+++ bountiful_foods/scripts/fresh.js
@@ -1,10 +1,14 @@
+
+
+
+//Adds event listeners for the number of orders
 const confirmation = document.querySelector(".orderConfirmation");
 const submit = document.getElementById("submitBtn");
 let count = localStorage.getItem("numOrders") || 0;
 
 submit.addEventListener("click", () => {
   const firstName = document.querySelector('input[name="first name"]').value;
-  const lastName = document.querySelector('input[name="last name"]').value;
+  const lastName = document.querySelector('input[name="email"]').value;
   const phone = document.querySelector('input[name="phone"]').value;
   if (firstName === "" || lastName === "" || phone === "") {
     alert("Please fill out all required fields in Contact Information");
@@ -14,8 +18,36 @@ submit.addEventListener("click", () => {
     count++;
     localStorage.setItem("numOrders", count);
     console.log(`Number of Submitted orders ${count}`);
+
+
+    //receives all the data from the form and puts it into the confirmation
+    const form = document.querySelector('form');
+    const formData = new FormData(form);
+
+    const firstName = formData.get('first name');
+    const email = formData.get('email');
+    const phone = formData.get('phone');
+    const size = formData.get('size');
+    const base = formData.get('base');
+    const fruit1 = formData.get('fruit1');
+    const fruit2 = formData.get('fruit2');
+    const fruit3 = formData.get('fruit3');
+    const specialInstructions = formData.get('specialInstructions');
+
+    document.getElementById("customerName").innerHTML = `<strong>Name:</strong> ${firstName}`
+    document.getElementById("customerEmail").innerHTML = `<strong>Email:</strong> ${email}`
+    document.getElementById("customerPhone").innerHTML = `<strong>Phone:</strong> ${phone}`
+    document.getElementById("drinkSize").innerHTML = `<strong>Drink Size:</strong> ${size}`
+    document.getElementById("drinkBase").innerHTML = `<strong>Base:</strong> ${base}`
+    document.getElementById("drinkFruit1").innerHTML = `<strong>Fruit 1:</strong> ${fruit1}`
+    document.getElementById("drinkFruit2").innerHTML = `<strong>Fruit 2:</strong> ${fruit2}`
+    document.getElementById("drinkFruit3").innerHTML = `<strong>Fruit 3:</strong> ${fruit3}`
+    document.getElementById("instructions").innerHTML = `<strong>Special Instructions:</strong> ${specialInstructions}`
+
   }
 });
+
+// Gets all the information for the fruits
 async function getFruit() {
   try {
     const response = await fetch(
@@ -118,6 +150,7 @@ async function getSugars() {
   }
 }
 
+//compiles fruits into dropdown
 async function fruitInfo() {
   const fruitNames = await getFruit();
   const calories = await getCalories();
