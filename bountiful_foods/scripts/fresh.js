099a538c498d2182480a356b6e1eee0f7f7afc61
Jester572 <jester572@yahoo.com>
2023-04-08 21:29:37 -0600

    Added javascript still debugging

---
+++ bountiful_foods/scripts/fresh.js
@@ -0,0 +1,102 @@
+async function getFruit() {
+    try {
+        const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+        if (response.ok) {
+            const data = await response.json()
+            fruitNames = data.map(fruit => fruit.name)
+        return fruitNames
+        } else {
+            throw Error(await response.text())
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getCarbohydrates() {
+    try {
+        const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+        if (response.ok) {
+            const data = await response.json()
+            fruitCarbohydrates = data.map(carbohydrates => carbohydrates.nutritions.carbohydrates)
+        return fruitCarbohydrates
+        } else {
+            throw Error(await response.text())
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getProtein() {
+    try {
+        const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+        if (response.ok) {
+            const data = await response.json()
+            fruitProtein = data.map(protein => protein.nutritions.protein)
+        return fruitProtein
+        } else {
+            throw Error(await response.text())
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function getFat() {
+    try {
+        const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+        if (response.ok) {
+            const data = await response.json()
+            fruitFat = data.map(fat => fat.nutritions.fat)
+        return fruitFat
+        } else {
+            throw Error(await response.text())
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function getCalories() {
+    try {
+        const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+        if (response.ok) {
+            const data = await response.json()
+            fruitCalories = data.map(calories => calories.nutritions.calories)
+        return fruitCalories
+        } else {
+            throw Error(await response.text())
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function getSugars() {
+    try {
+        const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+        if (response.ok) {
+            const data = await response.json()
+            fruitSugar = data.map(sugar => sugar.nutritions.sugar)
+            // console.log(fruitCalories)
+        return fruitSugar
+        } else {
+            throw Error(await response.text())
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function fruitInfo() {
+    const fruit = getFruit()
+    const calories = getCalories()
+    const carbs = getCarbohydrates()
+    const fat = getFat()
+    const sugars = getSugars()
+    const protein = getProtein()
+}
